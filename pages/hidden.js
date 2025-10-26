12f6ca3d18a016aa147100f31cbb82a7944bac2e
hwaniiidev <hwaniiidev@gmail.com>
2025-10-26 08:52:46 -0500

    modify portfolio url

--- pages/hidden.js
+++ pages/hidden.js
@@ -8,7 +8,7 @@ const HiddenPage = () => {
 
   const links = [
     { name: 'Resume', url: 'https://drive.google.com/file/d/14fbfLiYXCnpdUjSTpj-pSxA17gtP-8BZ/view?usp=sharing' },
-    { name: 'Portfolio', url: 'https://drive.google.com/file/d/1xG-E2LFBXB3APperaGRWVDgR1tef6ZoZ/view?usp=drive_link' },
+    { name: 'Portfolio', url: 'https://drive.google.com/file/d/1058YdKH2jUVhsDd-ByHBqoDecTGg9wfC/view' },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/seung-eun-lee-42ab71337' },
     { name: 'Google Drive', url: 'https://drive.google.com/drive/folders/15gY8ZFMs3LShg9Lr6TAx06FzUbaoCn2h?usp=drive_link' },
   ];
